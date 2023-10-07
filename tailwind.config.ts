603d7e4579305e241ef5b2397f45b12f2093f2db
Annie-Peng <djs910532@gmail.com>
2023-10-07 11:23:35 +0800

    feat: add css guideline (spacing, fontSize, fontWeight, borderRadius, and colors)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,61 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      spacing: {
+        4: "4px",
+        8: "8px",
+        10: "10px",
+        12: "12px",
+        16: "16px",
+        20: "20px",
+        24: "24px",
+      },
+      fontSize: {
+        12: "12px",
+        16: "16px",
+        18: "18px",
+        20: "20px",
+        24: "24px",
+      },
+      fontWeight: {
+        thin: "400",
+        normal: "500",
+        bold: "600",
+      },
+      borderRadius: {
+        10: "10px",
+        50: "50px",
+      },
+      colors: {
+        tertiary: {
+          //Mine Shaft - black
+          50: "#F6F6F6",
+          100: "#E7E7E7",
+          200: "#D1D1D1",
+          300: "#B0B0B0",
+          400: "#888888",
+          500: "#6D6D6D",
+          600: "#5D5D5D",
+          700: "#4F4F4F",
+          800: "#454545",
+          900: "#3D3D3D",
+          950: "#2D2D2D",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
