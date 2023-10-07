ff142b93e1d1ff9a4f4569758068068f8d5fd388
Annie-Peng <djs910532@gmail.com>
2023-10-07 18:10:22 +0800

    feat: create Layout page and LoginForm module

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/modules.tsx/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="container text-center">
+      <h2 className="text-24 font-normal mt-12">會員登入</h2>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
