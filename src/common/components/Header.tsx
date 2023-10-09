7adbdfd99d28ec94a74c35737c5aefd437efbe20
Annie-Peng <djs910532@gmail.com>
2023-10-09 17:00:19 +0800

    modify: add mobile  and adjust all style

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,25 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "public/images/logo.svg";
+import menu from "public/images/header/menu.svg";
 
 const Header = () => {
   return (
     <header className="bg-white">
-      <div className="container py-20 flex items-center">
-        <Image src={logo} width="221" height="38" alt="logo-NuCares" />
-        <nav className="ms-[64px] text-black-600">
-          <ul className="flex font-normal gap-x-[64px]">
+      <div className="container py-20 items-center grid grid-cols-4 lg:grid-cols-12 gap-12">
+        <Image
+          src={logo}
+          width="221"
+          height="38"
+          alt="logo-NuCares"
+          className="col-span-3"
+        />
+        <nav className=" text-black-600 sm:hidden lg:block col-span-4 -ms-[calc(110px-64px)]">
+          <ul className="flex font-normal">
             <li>
               <Link href="#">搜尋營養師</Link>
             </li>
             <li>
-              <Link href="#">成為NuCares營養師</Link>
+              <Link href="#" className="ms-[64px]">
+                成為NuCares營養師
+              </Link>
             </li>
           </ul>
         </nav>
-        <Link href="#" className="btn-cusSecondary ms-auto py-16 px-12">
+        <Link
+          href="#"
+          className="btn-cusSecondary ms-auto py-16 px-12  sm:hidden lg:block col-end-13"
+        >
           註冊/登入
         </Link>
+        <Image
+          src={menu}
+          width="24"
+          height="15"
+          alt="menu"
+          className="col-end-5 lg:hidden ms-auto"
+        />
       </div>
     </header>
   );
