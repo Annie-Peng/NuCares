fc735cc3a29d213de72761869fe8faced3678c90
Annie-Peng <djs910532@gmail.com>
2023-10-09 16:08:01 +0800

    modify: update Header component style

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "public/images/logo.svg";
 
 const Header = () => {
   return (
     <header className="bg-white">
       <div className="container py-20 flex items-center">
-        <Image src="/" width="150" height="50" alt="logo-NuCares" />
-        <nav>
-          <ul className="flex font-normal text-20">
+        <Image src={logo} width="221" height="38" alt="logo-NuCares" />
+        <nav className="ms-[64px] text-black-600">
+          <ul className="flex font-normal gap-x-[64px]">
             <li>
               <Link href="#">搜尋營養師</Link>
             </li>
@@ -16,7 +17,7 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <Link href="#" className="btn-cusPrimary ms-auto font-bold">
+        <Link href="#" className="btn-cusSecondary ms-auto py-16 px-12">
           註冊/登入
         </Link>
       </div>
