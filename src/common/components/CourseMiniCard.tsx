0ae8b09c1f5251265aed8cfd986a1a29ef7559e2
Annie-Peng <djs910532@gmail.com>
2023-10-10 13:24:28 +0800

    feat: create NutritionistListPage, NutritionistCard module, CourseMiniCard component

---
+++ src/common/components/CourseMiniCard.tsx
@@ -0,0 +1,11 @@
+const CourseMiniCard = () => {
+  return (
+    <div className="border px-16 py-10 rounded-15">
+      <h4>題驗 - 1週飲食建議</h4>
+      <p className="text-12">共2堂</p>
+      <h5 className="text-22 font-normal mt-24">NT$ 1,500</h5>
+    </div>
+  );
+};
+
+export default CourseMiniCard;
---
+++ src/modules.tsx/NutritionistCard.tsx
@@ -0,0 +1,34 @@
+import CourseMiniCard from "@/common/components/CourseMiniCard";
+
+const NutritionistCard = () => {
+  return (
+    <>
+      <div className="flex">
+        <img width='220' height='302' className="bg-white rounded-15"/>
+        <div className="content max-w-[454px] ms-22">
+          <h3 className="text-24 font-normal">陳瘦瘦 營養師</h3>
+          <img />
+          <ul className="w-fit flex gap-8 mt-12">
+            <li className="border rounded-50 py-2 px-10">體重控制</li>
+            <li className="border rounded-50 py-2 px-10">上班族營養</li>
+            <li className="border rounded-50 py-2 px-10">運動營養</li>
+          </ul>
+          <p className="mt-20">
+            致力於提供個人化的營養指導。憑藉多年的經驗，我幫助人們實現健康目標，提供專業的飲食建議和支持。讓我們一起走上健康之路，共同追求最佳的身體狀態！
+          </p>
+        </div>
+        <ul className="min-w-[272px] ms-46 flex flex-col gap-8">
+          <li>
+            <CourseMiniCard />
+          </li>
+          <li>
+            <CourseMiniCard />
+          </li>
+        </ul>
+      </div>
+      <p className="text-right -mt-16">了解更多課程>></p>
+    </>
+  );
+};
+
+export default NutritionistCard;
