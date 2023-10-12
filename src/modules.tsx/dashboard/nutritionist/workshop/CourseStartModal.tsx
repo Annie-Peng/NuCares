3f0a3cac2bce423460a2f1af6f43571aacd598af
Annie-Peng <djs910532@gmail.com>
2023-10-12 13:23:16 +0800

    feat: create CourseStartModal module

---
+++ src/modules.tsx/dashboard/nutritionist/workshop/CourseStartModal.tsx
@@ -0,0 +1,17 @@
+import MiniModal from "@/common/components/MiniModal";
+
+const CourseStartModal = () => {
+  return (
+    <MiniModal title="此課程期間為2023.10.1-2023.11.25">
+      <p className="text-center">確定開始課程？</p>
+      <button
+        type="button"
+        className="btn-cusSecondary block mx-auto mt-24 py-8 w-[250px]"
+      >
+        確定
+      </button>
+    </MiniModal>
+  );
+};
+
+export default CourseStartModal;
