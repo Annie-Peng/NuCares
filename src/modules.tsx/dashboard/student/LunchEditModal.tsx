325438b7a01ce5a47986ae4094c802afcbd20ba6
Annie-Peng <djs910532@gmail.com>
2023-10-13 14:43:20 +0800

    feat: create LunchEditModal module

---
+++ src/modules.tsx/dashboard/student/LunchEditModal.tsx
@@ -0,0 +1,12 @@
+import MealForm from "@/common/components/MealForm";
+import NormalModal from "@/common/components/NormalModal";
+
+const LunchEditModal = () => {
+  return (
+    <NormalModal title="午餐">
+      <MealForm />
+    </NormalModal>
+  );
+};
+
+export default LunchEditModal;
