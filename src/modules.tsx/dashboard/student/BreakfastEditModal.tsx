929d929c1888f7f7d73f5a3615f76f19664554e5
Annie-Peng <djs910532@gmail.com>
2023-10-13 14:40:42 +0800

    feat: create BreakfastEditModal module

---
+++ src/modules.tsx/dashboard/student/BreakfastEditModal.tsx
@@ -0,0 +1,12 @@
+import MealForm from "@/common/components/MealForm";
+import NormalModal from "@/common/components/NormalModal";
+
+const BreakfastEditModal = () => {
+  return (
+    <NormalModal title="早餐">
+      <MealForm />
+    </NormalModal>
+  );
+};
+
+export default BreakfastEditModal;
