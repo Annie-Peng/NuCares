82641e9087e5a6e5b33ed4af2bba23355c07bedb
Annie-Peng <djs910532@gmail.com>
2023-10-20 17:51:18 +0800

    feat: add sideBar Link

--- src/modules/dashboard/nutritionist/DashboardLayout.tsx
+++ src/modules/dashboard/nutritionist/DashboardLayout.tsx
@@ -1,24 +1,30 @@
 import { DashboardLayoutProps } from "@/types/interface";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const nutritionistTabs = [
   {
     tab: "我的學員",
+    tabURL: "/dashboard/nutritionist/student-list",
     iconURL: "/images/dashboard/dietary-record/sidebar-icons/course.svg",
   },
   {
     tab: "營養師專頁",
+    tabURL: "/dashboard/nutritionist/workshop",
     iconURL:
       "/images/dashboard/dietary-record/sidebar-icons/nutritionist/workshop.svg",
   },
   {
     tab: "帳戶",
+    tabURL: "/dashboard/nutritionist/account",
     iconURL:
       "/images/dashboard/dietary-record/sidebar-icons/nutritionist/account.svg",
   },
 ];
 
 const NutritionistDashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const [tab, setTab] = useState(nutritionistTabs[0]);
   return (
     <div className="container p-20 flex mt-[54px] rounded-50 max-w-[1210px] relative cusBackgroundBackdrop">
       <div className="w-[20%]">
@@ -40,14 +46,19 @@ const NutritionistDashboardLayout = ({ children }: DashboardLayoutProps) => {
         >
           {nutritionistTabs.map((nutritionistTab, index) => {
             return (
-              <li key={index} className="py-20 px-28 rounded-s-50">
-                <Image
-                  src={nutritionistTab.iconURL}
-                  width={20}
-                  height={20}
-                  className="inline-block align-middle mr-8"
-                />
-                <span className="align-middle">{nutritionistTab.tab}</span>
+              <li key={index}>
+                <Link
+                  href={nutritionistTab.tabURL}
+                  className="align-middle inline-block py-20 px-28 rounded-s-50 w-full"
+                >
+                  <Image
+                    src={nutritionistTab.iconURL}
+                    width={20}
+                    height={20}
+                    className="inline-block align-middle mr-8"
+                  />
+                  <span className="align-middle">{nutritionistTab.tab}</span>
+                </Link>
               </li>
             );
           })}
