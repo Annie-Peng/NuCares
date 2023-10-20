7c186bcf3f14654e8b2afe2bb1ccfb35df957365
Annie-Peng <djs910532@gmail.com>
2023-10-20 17:29:02 +0800

    modify: update NutritionistDashboardLayout style

--- src/modules/dashboard/nutritionist/DashboardLayout.tsx
+++ src/modules/dashboard/nutritionist/DashboardLayout.tsx
@@ -1,29 +1,59 @@
 import { DashboardLayoutProps } from "@/types/interface";
 import Image from "next/image";
 
+const nutritionistTabs = [
+  {
+    tab: "我的學員",
+    iconURL: "/images/dashboard/dietary-record/sidebar-icons/course.svg",
+  },
+  {
+    tab: "營養師專頁",
+    iconURL:
+      "/images/dashboard/dietary-record/sidebar-icons/nutritionist/workshop.svg",
+  },
+  {
+    tab: "帳戶",
+    iconURL:
+      "/images/dashboard/dietary-record/sidebar-icons/nutritionist/account.svg",
+  },
+];
+
 const NutritionistDashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <div className="container bg-red-300 p-20 flex min-h-[740px] mt-[30px] rounded-50">
-      <div className="w-[25%] ">
+    <div className="container p-20 flex mt-[54px] rounded-50 max-w-[1210px] relative cusBackgroundBackdrop">
+      <div className="w-[20%]">
         <div className="profile flex flex-col items-center">
           <Image
             src="/"
             width="100"
             height="100"
             alt="profile-photo"
-            className="rounded"
+            className="rounded-50 bg-black-200 border border-white"
           />
-          <p className="px-10 bg-white w-fit rounded-10 mt-16">營養師</p>
-          <p className="font-bold text-24">陳瘦瘦</p>
+          <p className="px-10 bg-primary-500 text-white w-fit rounded-10 mt-16 text-12 font-bold">
+            營養師
+          </p>
+          <p className="mt-4 text-24 font-normal relative">陳瘦瘦</p>
         </div>
-        <ul className="sideBar text-18 font-bold flex flex-col">
-          <li className="bg-white p-[25px] rounded-s-50 mt-[25px]">我的學員</li>
-          <li className="p-[25px]">我的營養師專頁</li>
-          <li className="p-[25px]">個人資料</li>
-          <li className="p-[25px]">帳戶</li>
+        <ul
+          className={`sideBar text-18 font-bold flex flex-col text-black-500 mt-32`}
+        >
+          {nutritionistTabs.map((nutritionistTab, index) => {
+            return (
+              <li key={index} className="py-20 px-28 rounded-s-50">
+                <Image
+                  src={nutritionistTab.iconURL}
+                  width={20}
+                  height={20}
+                  className="inline-block align-middle mr-8"
+                />
+                <span className="align-middle">{nutritionistTab.tab}</span>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div className="bg-white w-[75%] rounded-35 text-center p-20">
+      <div className="bg-white bg-opacity-50 w-[80%] rounded-35 text-center p-20">
         {children}
       </div>
     </div>
