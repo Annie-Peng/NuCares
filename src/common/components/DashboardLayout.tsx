dc84dcdcee48030d7afdd12c68259d55168c7445
Annie-Peng <djs910532@gmail.com>
2023-10-21 16:38:34 +0800

    feat: check isMobile to show Footer or not

--- src/common/components/DashboardLayout.tsx
+++ src/common/components/DashboardLayout.tsx
@@ -1,11 +1,21 @@
 import { DashboardLayoutProps } from "@/types/interface";
 import Header from "./Header";
+import Footer from "./Footer";
 
-const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+interface DashboardLayoutProps {
+  value: boolean;
+  children: ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({
+  value,
+  children,
+}) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="grow">{children}</main>
+      {value ? null : <Footer />}
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import DashboardContainer from "@/common/components/DashboardContainer";
 import DashboardContainerLayout from "@/common/components/DashboardContainerLayout";
 import DashboardLayout from "@/common/components/DashboardLayout";
 import Layout from "@/common/components/Layout";
+import useResize from "@/common/hooks/useResize";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const isMobile = useResize();
+
   const isDashboardPath = router.pathname.startsWith("/dashboard");
   return (
     <>
       {isDashboardPath ? (
-        <DashboardLayout>
+        <DashboardLayout value={isMobile}>
           <DashboardContainerLayout>
             <Component {...pageProps} />
           </DashboardContainerLayout>
