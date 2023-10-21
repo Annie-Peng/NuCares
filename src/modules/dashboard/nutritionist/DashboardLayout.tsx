f7917b9f6eeede08776df8ef71414b4133c236e8
Annie-Peng <djs910532@gmail.com>
2023-10-21 08:54:32 +0800

    modify: update tab switch by pathName === nutritionistTab.tabURL

--- src/modules/dashboard/nutritionist/DashboardLayout.tsx
+++ src/modules/dashboard/nutritionist/DashboardLayout.tsx
@@ -1,30 +1,35 @@
 import { DashboardLayoutProps } from "@/types/interface";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
 
 const nutritionistTabs = [
   {
     tab: "我的學員",
     tabURL: "/dashboard/nutritionist/student-list",
     iconURL: "/images/dashboard/dietary-record/sidebar-icons/course.svg",
+    iconName: "course.svg",
   },
   {
     tab: "營養師專頁",
     tabURL: "/dashboard/nutritionist/workshop",
     iconURL:
       "/images/dashboard/dietary-record/sidebar-icons/nutritionist/workshop.svg",
+    iconName: "workshop.svg",
   },
   {
     tab: "帳戶",
     tabURL: "/dashboard/nutritionist/account",
     iconURL:
       "/images/dashboard/dietary-record/sidebar-icons/nutritionist/account.svg",
+    iconName: "account.svg",
   },
 ];
 
 const NutritionistDashboardLayout = ({ children }: DashboardLayoutProps) => {
-  const [tab, setTab] = useState(nutritionistTabs[0]);
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <div className="container p-20 flex mt-[54px] rounded-50 max-w-[1210px] relative cusBackgroundBackdrop">
       <div className="w-[20%]">
@@ -46,17 +51,23 @@ const NutritionistDashboardLayout = ({ children }: DashboardLayoutProps) => {
         >
           {nutritionistTabs.map((nutritionistTab, index) => {
             return (
-              <li key={index}>
-                <Link
-                  href={nutritionistTab.tabURL}
-                  className="align-middle inline-block py-20 px-28 rounded-s-50 w-full"
-                >
-                  <Image
-                    src={nutritionistTab.iconURL}
-                    width={20}
-                    height={20}
-                    className="inline-block align-middle mr-8"
-                  />
+              <li
+                key={index}
+                className={
+                  pathname === nutritionistTab.tabURL
+                    ? "cusSelectTab"
+                    : "cusNoSelectTab"
+                }
+              >
+                <Link href={nutritionistTab.tabURL}>
+                  <div className="inline-block align-middle mr-8">
+                    <Image
+                      src={nutritionistTab.iconURL}
+                      width={20}
+                      height={20}
+                      alt={nutritionistTab.iconName}
+                    />
+                  </div>
                   <span className="align-middle">{nutritionistTab.tab}</span>
                 </Link>
               </li>
