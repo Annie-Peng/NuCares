93abdb1cb6f32ee3e73e0aee0eb84eebba911270
Annie-Peng <djs910532@gmail.com>
2023-10-22 19:37:38 +0800

    feat: add Provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/common/components/Layout";
+import store from "@/common/redux/sotre";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </Layout>
+    </Provider>
   );
 }
