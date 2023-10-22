07bcbe3a79fc6570a1bab3943dccbd26f5b69e08
Annie-Peng <djs910532@gmail.com>
2023-10-23 01:04:27 +0800

    feat: add student dashboard pages

---
+++ src/pages/dashboard/student/favorite.tsx
@@ -0,0 +1,7 @@
+import DashboardLayout from "@/common/components/DashboardLayout";
+
+const FavoritePage = () => {
+  return <DashboardLayout>FavoritePage</DashboardLayout>;
+};
+
+export default FavoritePage;
---
+++ src/pages/dashboard/student/update-password.tsx
@@ -0,0 +1,7 @@
+import DashboardLayout from "@/common/components/DashboardLayout";
+
+const UpdatePasswordPage = () => {
+  return <DashboardLayout>UpdatePasswordPage</DashboardLayout>;
+};
+
+export default UpdatePasswordPage;
