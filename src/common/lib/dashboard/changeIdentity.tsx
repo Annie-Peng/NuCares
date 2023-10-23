e5ecdf27f42f1d6a83457eafa76992a3c5e7f454
Annie-Peng <djs910532@gmail.com>
2023-10-23 12:13:03 +0800

    feat: add changeIdentity

---
+++ src/common/lib/dashboard/changeIdentity.tsx
@@ -0,0 +1,10 @@
+interface ChangeIdentity {
+  iconURL: string;
+  iconName: string;
+}
+
+const changeIdentity: ChangeIdentity = {
+  iconURL: "/images/dashboard/dietary-record/sidebar-icons/change-identity",
+  iconName: "change-identity.svg",
+};
+export default changeIdentity;
