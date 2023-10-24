06c69cf6477cbef0fe89cd783758a1bf386430cc
Annie-Peng <djs910532@gmail.com>
2023-10-24 22:49:47 +0800

    fix: add temporary userData

---
+++ src/common/lib/dashboard/user.tsx
@@ -0,0 +1,12 @@
+const userData = {
+  Id: "5", //資料庫id
+  RelName: "陳亮亮",
+  Email: "example@mail.com", // 回傳登入的email
+  ImgUrl:
+    "https://i.pinimg.com/564x/1f/37/39/1f373917adfdf156ad811330355e4456.jpg",
+  Token: "Token",
+  IsNutritionist: true,
+  currentID: "nutritionist",
+};
+
+export default userData;
