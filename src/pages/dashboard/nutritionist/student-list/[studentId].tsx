ea6a536dcde60b86c2e897eb2789097a3593a1a3
Annie-Peng <djs910532@gmail.com>
2023-10-24 09:44:35 +0800

    feat: add CourseInfo

--- src/pages/dashboard/nutritionist/student-list/[studentId].tsx
+++ src/pages/dashboard/nutritionist/student-list/[studentId].tsx
@@ -1,28 +1,36 @@
 import DashboardContainer from "@/common/components/DashboardContainer";
 import DailyDietary from "@/common/components/dietary-record/DailyDietary";
 import GoalCompletionRate from "@/common/components/dietary-record/GoalCompletionRate";
+import CourseInfo from "@/common/components/dietary-record/CourseInfo";
+import Image from "next/image";
+import { useState } from "react";
 
 const StudentIdPage = () => {
+  const [showInfo, setShowInfo] = useState<boolean>(false);
+
   return (
     <>
       <p className="text-left border-b w-fit">返回我的課程列表</p>
       <h2>我的紀錄</h2>
-      <div className="flex flex-wrap gap-12 w-full justify-center h-[622px]">
-        <div className="w-[30%]">
-          <DashboardContainer title="我的營養師">
-            <p>進階 - 8週飲食建議</p>
-            <div className="flex items-center justify-center">
-              <img className="rounded-full bg-black-300 w-[60px] h-[60px]" />
-              <p>陳瘦瘦</p>
-            </div>
-            <ul>
-              <li>iamthin@gmail.com</li>
-              <li>0922-333444</li>
-              <li>thinthingood</li>
-            </ul>
-          </DashboardContainer>
-        </div>
-        <div className="w-[68%]">
+      <div className="flex flex-wrap gap-12 w-full justify-center h-[622px] relative">
+        <button
+          className="absolute right-0 -top-[56px] p-10 btn-cusPrimaryInfo flex items-center gap-10"
+          onClick={() => setShowInfo(true)}
+        >
+          <Image
+            src="/images/dashboard/dietary-record/menu-white.svg"
+            width="20"
+            height="20"
+            alt="menu"
+          />
+          學員資訊
+        </button>
+        {showInfo && (
+          <div className="absolute top-0 right-0 z-10 cusShadow p-20 rounded-10 min-w-[240px]">
+            <CourseInfo />
+          </div>
+        )}
+        <div className="w-full">
           <DashboardContainer title="飲食紀錄">
             <DailyDietary />
           </DashboardContainer>
