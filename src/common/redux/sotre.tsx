330d2b66aa912ce6a13b2f01e481ce86d3b4ec0c
Annie-Peng <djs910532@gmail.com>
2023-10-28 11:10:36 +0800

    feat: add course api

--- src/common/redux/sotre.tsx
+++ src/common/redux/sotre.tsx
@@ -4,6 +4,8 @@ import authReducer from "./features/auth";
 
 import { register } from "./service/register";
 import { login } from "./service/login";
+import { course } from "./service/course";
+import { testCourse } from "./service/test/testCourse";
 
 const store = configureStore({
   reducer: {
@@ -11,9 +13,17 @@ const store = configureStore({
     registerPhases: registerReducer,
     [register.reducerPath]: register.reducer,
     [login.reducerPath]: login.reducer,
+    [course.reducerPath]: course.reducer,
+
+    [testCourse.reducerPath]: testCourse.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(register.middleware).concat(login.middleware),
+    getDefaultMiddleware()
+      .concat(register.middleware)
+      .concat(login.middleware)
+      .concat(course.middleware)
+
+      .concat(testCourse.middleware),
 });
 
 export default store;
