f71a8d369cefd06be3d1a082479a850b82851aa9
Annie-Peng <djs910532@gmail.com>
2023-10-30 02:28:59 +0800

    modify: move showModal from DashboardContainerLayout to DashboardLayout

--- src/common/components/DashboardLayout.tsx
+++ src/common/components/DashboardLayout.tsx
@@ -2,14 +2,26 @@ import { DashboardLayoutProps } from "@/types/interface";
 import Header from "./Header";
 import Footer from "./Footer";
 import { FC } from "react";
+import MenuEditModal from "@/modules/dashboard/nutritionist/student-list/MenuEditModal";
+import { useSelector } from "react-redux";
+import { selectShowModal } from "../redux/features/showModal";
+import CourseStartModal from "@/modules/dashboard/nutritionist/workshop/CourseStartModal";
+import BodyRateAddModal from "@/modules/dashboard/student/courses/BodyRateAddModal";
 
 const DashboardLayout: FC<DashboardLayoutProps> = ({ value, children }) => {
+  const showModal = useSelector(selectShowModal);
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="grow">{children}</main>
-      {value ? null : <Footer />}
-    </div>
+    <>
+      {showModal.MenuEditModal && <MenuEditModal />}
+      {showModal.CourseStartModal && <CourseStartModal />}
+      {showModal.BodyRateAddModal && <BodyRateAddModal />}
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="grow">{children}</main>
+        {value ? null : <Footer />}
+      </div>
+    </>
   );
 };
 
