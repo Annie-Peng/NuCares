bfe5ad102b86b0f2718e98120b747b7f950aabd1
Annie-Peng <djs910532@gmail.com>
2023-10-29 09:15:42 +0800

    modify: update GoalCompletionRate style

--- src/common/components/dietary-record/GoalCompletionRate.tsx
+++ src/common/components/dietary-record/GoalCompletionRate.tsx
@@ -13,21 +13,40 @@ const BMIChart = dynamic(() => import("./BMIGoalChart"), {
 
 const GoalCompletionRate = () => {
   return (
-    <div className="flex flex-col justify-between pt-[65px] h-full">
-      <ul className="flex justify-between">
-        <li className="w-1/2">
-          <WeightChart />
-          <p>目標體重</p>
-          <p>50 公斤</p>
-        </li>
-        <li className="w-1/2">
-          <BMIChart />
-          <p>目標體脂</p>
-          <p>30 %</p>
-        </li>
-      </ul>
-      <button className="btn-cusSecondary">編輯</button>
-    </div>
+    <ul className="flex justify-center gap-[44px]">
+      <li className="flex flex-col gap-16">
+        <p className="cusPrimaryTag px-26">體重</p>
+        <p className="text-14 text-primary-500">
+          目標
+          <br />
+          <span className="text-24 font-bold">50</span>
+          <span className="text-12">公斤</span>
+        </p>
+        <WeightChart />
+        <p className="text-14 text-black-400">
+          現在
+          <br />
+          <span className="text-24 font-bold">80</span>
+          <span className="text-12">公斤</span>
+        </p>
+      </li>
+      <li className="flex flex-col gap-16">
+        <p className="cusPrimaryTag px-26">體脂</p>
+        <p className="text-14 text-primary-500">
+          目標
+          <br />
+          <span className="text-24 font-bold">30</span>
+          <span className="text-12">公斤</span>
+        </p>
+        <BMIChart />
+        <p className="text-14 text-black-400">
+          現在
+          <br />
+          <span className="text-24 font-bold">80</span>
+          <span className="text-12">公斤</span>
+        </p>
+      </li>
+    </ul>
   );
 };
 
