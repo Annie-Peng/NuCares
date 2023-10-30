56e0dac13a34e43dbb9f3f9c29316a1283cdde73
Annie-Peng <djs910532@gmail.com>
2023-10-30 16:18:08 +0800

    modify: update course api

--- src/common/redux/service/course.tsx
+++ src/common/redux/service/course.tsx
@@ -1,24 +1,17 @@
-import { RootState } from "@/types/interface";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const course = createApi({
   reducerPath: "course",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_URL,
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.Token;
-
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-    },
   }),
   endpoints: (builder) => ({
     courseListGetApi: builder.query({
-      query: ({ UserCurrentStatus, PageId }) => ({
+      query: ({ Token, UserCurrentStatus, PageId }) => ({
         url: `/${UserCurrentStatus}/courses?page=${PageId}`,
         method: "GET",
         headers: {
+          Authorization: `${Token}`,
           "Content-Type": "application/json",
         },
       }),
