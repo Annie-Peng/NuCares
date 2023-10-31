352cfeef7e66bcbbd376a859b24a050de6d67bb2
Annie-Peng <djs910532@gmail.com>
2023-10-31 15:55:23 +0800

    feat: add changeIDSlice & courseRecord api

--- src/common/redux/store.tsx
+++ src/common/redux/store.tsx
@@ -3,26 +3,31 @@ import { createWrapper } from "next-redux-wrapper";
 import registerReducer from "./features/registerPhases";
 import authReducer from "./features/auth";
 import showModalReducer from "./features/showModal";
+import changeIDReducer from "./features/changeID";
 
 import { register } from "./service/register";
 import { login } from "./service/login";
 import { course } from "./service/course";
+import { courseRecord } from "./service/courseRecord";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     registerPhases: registerReducer,
     showModal: showModalReducer,
+    // changeID: changeIDReducer,
 
     [register.reducerPath]: register.reducer,
     [login.reducerPath]: login.reducer,
     [course.reducerPath]: course.reducer,
+    [courseRecord.reducerPath]: courseRecord.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(register.middleware)
       .concat(login.middleware)
-      .concat(course.middleware),
+      .concat(course.middleware)
+      .concat(courseRecord.middleware),
 });
 
 const wrapper = createWrapper(() => store, { debug: false });
