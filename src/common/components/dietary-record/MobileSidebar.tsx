4b35ac11a0a9fb87d461485a4d44cb25250254b2
Annie-Peng <djs910532@gmail.com>
2023-11-01 21:48:13 +0800

    modify: update MobileSidebar

--- src/common/components/dietary-record/MobileSidebar.tsx
+++ src/common/components/dietary-record/MobileSidebar.tsx
@@ -8,43 +8,45 @@ interface MobileSidebarProps {
 
 const MobileSidebar = ({ showTab, setShowTab }: MobileSidebarProps) => {
   return (
-    <ul className="fixed bottom-0 left-0 right-0 bg-primary-400 h-[65px] flex justify-center items-center gap-20 px-[25px] lg:hidden">
-      {dietaryRecordTabs.map((tab, index) => {
-        return showTab === index + 1 ? (
-          <li key={index}>
-            <button
-              type="button"
-              className="text-center flex flex-col justify-center items-center relative cusShadow rounded-[38px] w-[70px] h-[70px] -mt-[5px]"
-              onClick={() => setShowTab(index + 1)}
-            >
-              <Image
-                src={`${tab.iconURL}-choose.svg`}
-                width={30}
-                height={30}
-                alt={tab.iconName}
-              />
-              <h4 className="text-14 text-primary-600">{tab.tab}</h4>
-            </button>
-          </li>
-        ) : (
-          <li key={index}>
-            <button
-              type="button"
-              className="text-center flex flex-col justify-center items-center"
-              onClick={() => setShowTab(index + 1)}
-            >
-              <Image
-                src={`${tab.iconURL}.svg`}
-                width={30}
-                height={30}
-                alt={tab.iconName}
-              />
-              <h4 className="text-14 text-white">{tab.tab}</h4>
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="bg-primary-400 fixed bottom-0 left-0 right-0 ">
+      <ul className="h-[65px] flex justify-between items-center px-[25px] bg-primary-400 container lg:hidden">
+        {dietaryRecordTabs.map((tab, index) => {
+          return showTab === index + 1 ? (
+            <li key={index}>
+              <button
+                type="button"
+                className="text-center flex flex-col justify-center items-center relative cusMobileSidebarShadow rounded-[38px] w-[70px] h-[70px] -mt-[5px]"
+                onClick={() => setShowTab(index + 1)}
+              >
+                <Image
+                  src={`${tab.iconURL}-choose.svg`}
+                  width={30}
+                  height={30}
+                  alt={tab.iconName}
+                />
+                <h4 className="text-14 text-primary-600">{tab.tab}</h4>
+              </button>
+            </li>
+          ) : (
+            <li key={index}>
+              <button
+                type="button"
+                className="text-center flex flex-col justify-center items-center"
+                onClick={() => setShowTab(index + 1)}
+              >
+                <Image
+                  src={`${tab.iconURL}.svg`}
+                  width={30}
+                  height={30}
+                  alt={tab.iconName}
+                />
+                <h4 className="text-14 text-white">{tab.tab}</h4>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
