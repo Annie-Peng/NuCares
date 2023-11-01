0c201de1fbe1d402b293dc5f87095268e1275f3a
Annie-Peng <djs910532@gmail.com>
2023-11-01 16:50:54 +0800

    modify: update BodyRateAddModal

--- src/common/lib/dashboard/dietary-record/bodyRate.tsx
+++ src/common/lib/dashboard/dietary-record/bodyRate.tsx
@@ -9,13 +9,15 @@ interface BodyRateTabsType {
   enName: string;
 }
 
-export const bodyRateAdd: BodyRateAddType[] = [
+const bodyRateAdd: BodyRateAddType[] = [
   { name: "身高", enName: "Height", unit: "公分" },
   { name: "體重", enName: "Weight", unit: "公斤" },
   { name: "體脂", enName: "BodyFat", unit: "%" },
   { name: "內臟脂肪", enName: "VisceralFat", unit: "級" },
   { name: "骨骼肌率", enName: "SMM", unit: "%" },
 ];
+
+export default bodyRateAdd;
 
 export const bodyRateTabs: BodyRateTabsType[] = [
   { name: "身高", enName: "Height" },
--- src/modules/dashboard/student/courses/BodyRateAddModal.tsx
+++ src/modules/dashboard/student/courses/BodyRateAddModal.tsx
@@ -1,35 +1,83 @@
 import TitleModal from "@/common/components/TitleModal";
-import { bodyRateAdd } from "@/common/lib/dashboard/dietary-record/bodyRate";
+import bodyRateAdd from "@/common/lib/dashboard/dietary-record/bodyRate";
+import { closeModal } from "@/common/redux/features/showModal";
+import { useBodyInfoPostApiMutation } from "@/common/redux/service/courseRecord";
 import { FC } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
 interface BodyRateAddModalProps {
-  data: string;
+  data: {
+    Token: string;
+    CourseId: string;
+    [key: string]: string;
+  };
+}
+
+export interface FormInput {
+  Height: string;
+  Weight: string;
+  BodyFat: string;
+  VisceralFat: string;
+  SMM: string;
+  Bmi: string;
+  Bmr: string;
+  [key: string]: string;
 }
 
 const BodyRateAddModal: FC<BodyRateAddModalProps> = ({ data }) => {
+  const [bodyInfoPostApi] = useBodyInfoPostApiMutation();
+  const dispatch = useDispatch();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormInput>();
+
+  const onSubmit: SubmitHandler<FormInput> = async (formData) => {
+    try {
+      const result = await bodyInfoPostApi({
+        Token: data.Token,
+        CourseId: data.CourseId,
+        body: formData,
+      }).unwrap();
+      dispatch(closeModal("showBodyRateAddModal"));
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <TitleModal title="今天身體數值" width="820px" modal="showBodyRateAddModal">
-      <form>
-        <ul className="flex gap-32 my-32">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ul className="flex gap-32 my-32 text-center">
           {bodyRateAdd.map((item, index) => (
             <li key={index}>
-              <label htmlFor={item.enName} className="relative">
+              <label className="relative">
                 <p className="bg-primary-400 text-white rounded-35">
                   {item.name}
                 </p>
                 <input
-                  name={item.enName}
                   type="number"
                   className="w-full mt-8 pr-[42px]"
+                  {...register(`${item.enName}`, { required: "*必填" })}
                 />
                 <p className="absolute right-12 top-[42px] text-black-400">
                   {item.unit}
                 </p>
               </label>
+              <p className="text-left text-secondary-600 mt-4">
+                {errors[item.enName]?.message}
+              </p>
             </li>
           ))}
         </ul>
-        <button type="submit" className="btn-cusSecondary p-6 w-[270px]">
+        <button
+          type="submit"
+          className="btn-cusSecondary p-6 w-[270px] block mx-auto"
+        >
           新增
         </button>
       </form>
