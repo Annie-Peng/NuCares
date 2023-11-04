607a417182c96da97da203d47693efad470df282
Annie-Peng <djs910532@gmail.com>
2023-11-04 16:01:01 +0800

    modify: update NutritionistListPage, NutritionistCard, CourseMiniCard

--- src/pages/nutritionist-list/index.tsx
+++ src/pages/nutritionist-list/index.tsx
@@ -4,35 +4,39 @@ const NutritionistListPage = () => {
   return (
     <div className="container grid cusGrid">
       <div className="col-start-2 col-span-10">
-        <div className="text-center text-20 font-bold">選擇營養師專長</div>
-        <ul className="flex justify-between gap-x-10 text-center mt-28 text-20 font-bold">
-          <li className="py-[25px] border w-full rounded-15">
+        <div className="text-center text-20 font-bold">
+          為您找到合適的營養師
+        </div>
+        <ul className="flex justify-between gap-x-10 text-center mt-24 text-20 font-bold">
+          <li className="!p-20 w-full btn-cusWritePrimary text-18 !rounded-15 bg-white flex justify-center items-center">
             <h2>體重控制</h2>
           </li>
-          <li className="py-[25px] border w-full rounded-15">
+          <li className="!p-20 w-full btn-cusWritePrimary text-18 !rounded-15 bg-white flex justify-center items-center">
             <h2>上班族營養</h2>
           </li>
-          <li className="py-[25px] border w-full rounded-15">
+          <li className="!p-20 w-full btn-cusWritePrimary text-18 !rounded-15 bg-white flex justify-center items-center">
             <h2>孕期營養</h2>
           </li>
-          <li className="py-[25px] border w-full rounded-15">
+          <li className="!p-20 w-full btn-cusWritePrimary text-18 !rounded-15 bg-white flex justify-center items-center">
             <h2>樂玲營養與保健</h2>
           </li>
         </ul>
-        <div className="mt-80 flex justify-end items-center">
+        <div className="mt-[36px] flex justify-end items-center">
           <span className="font-bold">排列依序</span>
           <ul className="flex gap-8 ms-8">
-            <li className="border rounded-50 py-4 px-10">價格最高</li>
-            <li className="border rounded-50 py-4 px-10">價格最低</li>
-            <li className="border rounded-50 py-4 px-10">評價最高</li>
-            <li className="border rounded-50 py-4 px-10">平價數量最多</li>
+            <li className="border border-black-950 rounded-50 py-4 px-10">
+              價格最低
+            </li>
+            <li className="border border-black-950 rounded-50 py-4 px-10">
+              評價最高
+            </li>
+            <li className="border border-black-950 rounded-50 py-4 px-10">
+              評價數量最多
+            </li>
           </ul>
         </div>
         <ul>
-          <li className="bg-red-500 rounded-20 mt-28 p-28">
-            <NutritionistCard />
-          </li>
-          <li className="bg-red-500 rounded-20 mt-28 p-28">
+          <li className="bg-white rounded-20 mt-16 p-40 flex gap-20">
             <NutritionistCard />
           </li>
         </ul>
