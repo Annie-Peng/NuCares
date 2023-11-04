beebd79511bf5c04157f241fd59a543d402d6061
Annie-Peng <djs910532@gmail.com>
2023-11-05 00:44:23 +0800

    modify: update NutritionistIdPage, NutritionistIntro, CourseNormalCard

--- src/pages/nutritionist-list/[nutritionistId].tsx
+++ src/pages/nutritionist-list/[nutritionistId].tsx
@@ -3,12 +3,12 @@ import NutritionistIntro from "@/modules/NutritionistIntro";
 
 const NutritionistIdPage = () => {
   return (
-    <div className="container cusGrid grid mt-74">
-      <div className="col-start-2 col-span-6 bg-black-300 h-screen flex flex-col gap-14">
+    <div className="container cusGrid grid my-24">
+      <div className="col-start-2 col-span-6 flex flex-col gap-16">
         <NutritionistIntro />
       </div>
-      <div className="col-end-12 col-span-4 bg-primary-300 h-screen">
-        <ul className="flex flex-col gap-22">
+      <div className="col-end-12 col-span-4 bg-white p-20 self-start rounded-20">
+        <ul className="flex flex-col gap-10">
           <li>
             <CourseNormalCard />
           </li>
