8bb31d1ea4d3ab7a83690f51966878c6c5177fa3
Annie-Peng <djs910532@gmail.com>
2023-11-05 01:30:12 +0800

    modify: CourseMiniCard border-primary-200

--- src/modules/CourseMiniCard.tsx
+++ src/modules/CourseMiniCard.tsx
@@ -1,6 +1,6 @@
 const CourseMiniCard = () => {
   return (
-    <div className="border border-primary-100 p-20 rounded-15 relative">
+    <div className="border border-primary-200 p-20 rounded-15 relative">
       <h4>題驗 - 1週飲食建議</h4>
       <p className="text-14 mt-4">共1週</p>
       <h5 className="text-22 font-normal mt-12">NT$ 1,500</h5>
