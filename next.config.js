9145ea9c2a3121bebf0fa4a27de0d041968082b1
Annie-Peng <djs910532@gmail.com>
2023-11-06 10:57:39 +0800

    feat: add images remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "3acfedeb.r6.cpolar.top",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
