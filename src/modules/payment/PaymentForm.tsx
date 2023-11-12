5545a8c8c442688f97d6db4750f3cb49a6187a70
Annie-Peng <djs910532@gmail.com>
2023-11-12 21:58:18 +0800

    feat: add PaymentForm validation & move to src/modules/payment

--- src/modules/payment/PaymentForm.tsx
+++ src/modules/payment/PaymentForm.tsx
@@ -1,26 +1,57 @@
+import { Auth } from "@/common/redux/features/auth";
 import { usePaymentGetApiQuery } from "@/common/redux/service/payment";
 import Image from "next/image";
-import Link from "next/link";
 import paymentStep1 from "public/images/payment/paymentStep1.svg";
 import { FC } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface PaymentFormProps {
-  Token: string;
+  auth: Auth;
   planId: string;
 }
 
-const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
+export interface FormInput {
+  UserName: string;
+  UserEmail: string;
+  UserLineId: string;
+  UserPhone: string;
+  ContactTime: string;
+  Invoice: string;
+  PaymentMethod: string;
+}
+
+const PaymentForm: FC<PaymentFormProps> = ({ auth, planId }) => {
   const {
     data: renderData,
     isLoading,
     error,
-  } = usePaymentGetApiQuery({ Token, planId });
+  } = usePaymentGetApiQuery({ Token: auth.Token, planId });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormInput>({
+    defaultValues: {
+      UserName: auth.UserName,
+      UserEmail: auth.Email,
+      UserLineId: "",
+      UserPhone: "",
+      ContactTime: "",
+      Invoice: "",
+      PaymentMethod: "",
+    },
+  });
 
   console.log(renderData);
 
   if (isLoading || !renderData) {
     return <p>Payment is Loading</p>;
   }
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -33,9 +64,12 @@ const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
           alt="paymentStep1"
         />
       </div>
-      <form className=" bg-white flex flex-wrap justify-center text-left mt-16 p-40 rounded-20 gap-x-[200px]">
+      <form
+        className=" bg-white flex flex-wrap justify-center text-left mt-16 p-40 rounded-20 gap-x-[200px]"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="w-[30%] flex flex-col gap-12">
-          <div className="flex gap-8 items-center">
+          <div className="flex flex-wrap gap-8 items-center lg:flex">
             <h3 className="text-20 font-normal">訂購人資料</h3>
             <span className="text-14 text-black-400">
               此份資料會提供給營養師
@@ -45,8 +79,9 @@ const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
             <p className="font-bold">姓名</p>
             <input
               className="cusInputNoIcon mt-12 w-full"
-              name="UserName"
               type="text"
+              value={auth.UserName}
+              {...register("UserName")}
               disabled
             />
           </label>
@@ -54,33 +89,52 @@ const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
             <p className="font-bold">E-mail</p>
             <input
               className="cusInputNoIcon mt-12 w-full"
-              name="UserEmail"
               type="email"
+              value={auth.Email}
+              {...register("UserEmail")}
               disabled
             />
           </label>
           <label htmlFor="UserPhone" className="w-full">
-            <p className="font-bold">手機號碼</p>
+            <p className="font-bold">手機號碼*</p>
             <input
               className="cusInputNoIcon mt-12 w-full"
-              name="UserPhone"
               type="number"
+              {...register("UserPhone", {
+                required: "*必填",
+                pattern: {
+                  value: /^09\d{8}$/,
+                  message: "手機號碼格式有誤",
+                },
+              })}
             />
           </label>
+          <p className="text-secondary-600 -mt-8">
+            {errors.UserPhone?.message}
+          </p>
           <label htmlFor="UserLineId" className="w-full">
             <p className="font-bold">Line ID*</p>
             <input
               className="cusInputNoIcon mt-12 w-full"
-              name="UserLineId"
               type="text"
+              {...register("UserLineId", {
+                required: "*必填",
+                pattern: {
+                  value: /[a-zA-Z\d]/,
+                  message: "Line格式有誤",
+                },
+              })}
             />
           </label>
+          <p className="text-secondary-600 -mt-8">
+            {errors.UserLineId?.message}
+          </p>
           <label htmlFor="ContactTime" className="relative w-[174px]">
             <p className="font-bold">方便聯絡時間*</p>
             <p className="text-black-400 text-14">營養師將會與您聯繫</p>
             <select
               className="cusInputWithNoIcon mt-12 pr-32"
-              name="ContactTime"
+              {...register("ContactTime", { required: "*必填" })}
             >
               <option value="" disabled selected>
                 請選擇
@@ -91,6 +145,9 @@ const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
             </select>
             <div className="bg-arrowDownIcon w-20 h-20 content-[''] absolute bottom-14 right-12 block" />
           </label>
+          <p className="text-secondary-600 -mt-8">
+            {errors.ContactTime?.message}
+          </p>
         </div>
         <div className="w-full flex flex-col gap-12 lg:w-[30%]">
           <div className="p-20 border rounded-15 border-black-300 flex flex-col gap-4">
@@ -104,28 +161,40 @@ const PaymentForm: FC<PaymentFormProps> = ({ Token, planId }) => {
             </p>
           </div>
           <div className="p-20 border rounded-15 border-black-300">
-            <h3 className="text-18 font-bold">付款方式</h3>
+            <div className="flex justify-between">
+              <h3 className="text-18 font-bold">付款方式</h3>
+              <span className="text-secondary-600">
+                {errors.PaymentMethod?.message}
+              </span>
+            </div>
             <hr className="mt-12 border-black-300" />
             <label htmlFor="PaymentMethod" className="mt-12 block">
               <input
-                name="PaymentMethod"
                 type="radio"
                 id="PaymentMethod"
                 className="hidden"
+                value="信用卡"
+                {...register("PaymentMethod", { required: "*請勾選" })}
               />
               <span className="w-22 h-22 inline-block border border-black-950 rounded-50 align-middle" />
               <span className="ml-12 align-middle">信用卡</span>
             </label>
           </div>
           <div className="p-20 border rounded-15 border-black-300">
-            <h3 className="text-18 font-bold">發票</h3>
+            <div className="flex justify-between">
+              <h3 className="text-18 font-bold">發票</h3>
+              <span className="text-secondary-600">
+                {errors.Invoice?.message}
+              </span>
+            </div>
             <hr className="mt-12 border-black-300" />
             <label htmlFor="Invoice" className="mt-12 block">
               <input
-                name="Invoice"
                 type="radio"
                 id="Invoice"
                 className="hidden"
+                value="平台電子發票"
+                {...register("Invoice", { required: "*請勾選" })}
               />
               <span className="w-22 h-22 inline-block border border-black-950 rounded-50 align-middle" />
               <span className="ml-12 align-middle">平台電子發票</span>
