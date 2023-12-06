0224309d7adb2812d9635a47e34fbdbe10194ddd
Annie-Peng <djs910532@gmail.com>
2023-12-06 09:35:35 +0800

    modify: update LifeSurveyForm style

--- src/modules/dashboard/nutritionist/student-list/LifeSurveyForm.tsx
+++ src/modules/dashboard/nutritionist/student-list/LifeSurveyForm.tsx
@@ -26,92 +26,109 @@ const LifeSurveyForm: FC<LifeSurveyFormProps> = ({
       : "/dashboard/nutritionist/student-list";
 
   return (
-    <div className="container">
+    <>
       <Link
         href={routeListPage}
         className="before:content-['<'] before:mr-4 text-left border-black-950 border-b w-fit hidden lg:block"
       >
         返回我的課程列表
       </Link>
-      <h3 className="cusPrimaryTitle mt-20">學員飲食生活問卷</h3>
-      <div className="container text-left flex flex-col cusDashboardInnerContainer mt-32 p-20">
-        <ul className="flex gap-24 px-20">
-          <li>姓名：{studentData.UserName}</li>
-          <li>性別：{studentData.Gender}</li>
-          <li>年齡：{studentData.Age}</li>
-        </ul>
-        <div className="mt-12 flex flex-col gap-20">
-          {studentData.Answers.map((answer, aIndex) => {
-            const questionFieldTitle = lifeSurveyTabs[aIndex].title;
-            let questionNumSet: string[] = [];
-            let questionDataSet: Record<
-              string,
-              string | string[] | undefined
-            >[] = [];
-            let answerValueSet: string[][] = [];
-            Object.entries(answer).forEach(([key, value]) => {
-              const questionNum = key.split("Question")[1];
-              questionNumSet.push(questionNum);
-              questionDataSet.push(lifeSurveyData[Number(questionNum) - 1]);
-              answerValueSet.push(value);
-            });
-            return (
-              <div key={aIndex} className="flex flex-col border rounded-15">
-                <h4 className="py-12 px-16 cusPrimaryTitle text-18 after:content-[''] after:block after:h-[7px] after:bg-primary-100 after:-mt-10">
-                  {questionFieldTitle}
-                </h4>
-                <div className="p-20">
-                  {questionDataSet.map((question, qIndex) => {
-                    return (
-                      <div key={`field${aIndex}-question${qIndex}`}>
-                        {qIndex > 0 && <hr className="my-20" />}
-                        <h5 className="font-bold">{question.title}</h5>
-                        <ul className="mt-12">
-                          {answerValueSet[qIndex]?.map((value, vIndex) => {
-                            const hasValue = value !== "";
+      <h2 className="cusPrimaryTitle p-20 text-white bg-primary-400 lg:text-primary-500 lg:p-0 lg:mt-20 lg:bg-transparent">
+        學員飲食生活問卷
+      </h2>
+      <div className="container relative">
+        <Link
+          href="/dashboard/nutritionist/student-list"
+          className="absolute left-30 -top-[52px] w-[36px] h-[36px] lg:hidden"
+        >
+          <Image
+            src="/images/dashboard/dietary-record/back.svg"
+            layout="fixed"
+            width={36}
+            height={36}
+            alt="back.svg"
+          />
+        </Link>
+        <div className="text-left flex flex-col cusDashboardInnerContainer mt-0 p-20 lg:mt-32">
+          <ul className="flex gap-24 px-20">
+            <li>姓名：{studentData.UserName}</li>
+            <li>性別：{studentData.Gender}</li>
+            <li>年齡：{studentData.Age}</li>
+          </ul>
+          <div className="mt-12 flex flex-col gap-20">
+            {studentData.Answers.map((answer, aIndex) => {
+              const questionFieldTitle = lifeSurveyTabs[aIndex].title;
+              let questionNumSet: string[] = [];
+              let questionDataSet: Record<
+                string,
+                string | string[] | undefined
+              >[] = [];
+              let answerValueSet: string[][] = [];
+              Object.entries(answer).forEach(([key, value]) => {
+                const questionNum = key.split("Question")[1];
+                questionNumSet.push(questionNum);
+                questionDataSet.push(lifeSurveyData[Number(questionNum) - 1]);
+                answerValueSet.push(value);
+              });
+              return (
+                <div key={aIndex} className="flex flex-col border rounded-15">
+                  <h3 className="py-12 px-16 cusPrimaryTitle text-18 after:content-[''] after:block after:h-[7px] after:bg-primary-100 after:-mt-10">
+                    {questionFieldTitle}
+                  </h3>
+                  <div className="p-20">
+                    {questionDataSet.map((question, qIndex) => {
+                      return (
+                        <div key={`field${aIndex}-question${qIndex}`}>
+                          {qIndex > 0 && <hr className="my-20" />}
+                          <h4 className="font-bold">{question.title}</h4>
+                          <ul className="mt-12 flex flex-col gap-8">
+                            {answerValueSet[qIndex]?.map((value, vIndex) => {
+                              const hasValue = value !== "";
 
-                            return (
-                              <li
-                                key={`field${aIndex}-question${qIndex}-value${vIndex}`}
-                                className="flex items-center gap-8"
-                              >
-                                {hasValue ? (
-                                  <>
-                                    {question.method === "single-choice" && (
-                                      <Image
-                                        src="/images/dashboard/nutritionist/course/life-survey/radio.svg"
-                                        width={20}
-                                        height={20}
-                                        alt="radio"
-                                      />
-                                    )}
-                                    {question.method === "multiple-choices" && (
-                                      <Image
-                                        src="/images/dashboard/nutritionist/course/life-survey/checked.svg"
-                                        width={20}
-                                        height={20}
-                                        alt="checked"
-                                      />
-                                    )}
-                                    {value}
-                                  </>
-                                ) : (
-                                  <p className="text-black-300">無填寫</p>
-                                )}
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </div>
-                    );
-                  })}
+                              return (
+                                <li
+                                  key={`field${aIndex}-question${qIndex}-value${vIndex}`}
+                                  className="flex items-center gap-8"
+                                >
+                                  {hasValue ? (
+                                    <>
+                                      {question.method === "single-choice" && (
+                                        <Image
+                                          src="/images/dashboard/nutritionist/course/life-survey/radio.svg"
+                                          width={20}
+                                          height={20}
+                                          alt="radio"
+                                        />
+                                      )}
+                                      {question.method ===
+                                        "multiple-choices" && (
+                                        <Image
+                                          src="/images/dashboard/nutritionist/course/life-survey/checked.svg"
+                                          width={20}
+                                          height={20}
+                                          alt="checked"
+                                        />
+                                      )}
+                                      {value}
+                                    </>
+                                  ) : (
+                                    <p className="text-black-300">無填寫</p>
+                                  )}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
